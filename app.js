15cc39adfe4a8a3acda3315ec5fd71e979f9fd0e
Abdulla Mian <woodworks87@gmail.com>
2016-11-25 23:40:52 -0600

    local storage

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-$(document).ready(function(){
+var ToDoApp = {};
 
-	// Add item to list
+$(document).ready(function() {
+
+	var toDoStorage = new Storage('todoList')
+
+	if (!toDoStorage.getValue()) {
+		toDoStorage.initialize();
+	}
 
 	$(".submit_form").on('click', function(event){
-		
+
 		event.preventDefault();
-		var user_todo = $(".text_box").val();
-		$(".list").append('<li> <span>' + user_todo + '</span> <span> X </span> </li>');
-
-	});
-		
-	
-	// Delete item from list
-
-	$(this).on('click', function(){
-		alert("hey");
-
+		var toDo = $(".text_box").val();
+		toDoStorage.putValue(toDo);
+		$(".list").append('<li> <span>' + toDo + '</span> <img id="deleteImg" src="images/delete.png" /> </li>');
 
 	});
 
-	// $(".list > li:last-child > span:nth-child(2)").on('click', function(){
-	
-	// 	alert("delete");
-	
-	// });
-
-		// 1. Delete should be functioning.
-		// 	remove the item clicked on.
-		// 2. Organize the code
-		// 	break into two functions add item / remove item
-		// 3. add or remove items in the browser database (local store & index db)
-		// 	 load items from local storage
+	$(document).on('click', 'li', function(){
+		$(this).remove();
+		toDoStorage.removeValue(this.innerText.trim());
+	});
 });
---
+++ index.js
@@ -0,0 +1,28 @@
+var Storage = function (key) {
+
+	this.key = key;
+
+	this.putValue = function (value) {
+		var data = JSON.parse(localStorage.getItem(this.key));
+		data.push(value);
+		return localStorage.setItem(this.key, JSON.stringify(data));
+	}
+
+	this.getValue = function () {
+		return JSON.parse(localStorage.getItem(this.key));
+	}
+
+	this.initialize = function () {
+		return localStorage.setItem(this.key, JSON.stringify([])); 
+	} 
+
+	this.removeValue = function (value) {
+		var findValues  = this.getValue();
+		var findIndex = findValues.indexOf(value);
+		if (findIndex !== -1) {
+			findValues.splice(findIndex, 1)
+		    return localStorage.setItem(this.key, JSON.stringify(findValues));
+		}
+	}
+
+}
